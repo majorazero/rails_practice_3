e1ce5f9d29c1ca37a7b59c6e640486637f643f21
majorazero <daniel.cw.hsu@gmail.com>
2019-03-17 18:34:47 -0700

    simple model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name:'example',email:'example@example.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
 end
