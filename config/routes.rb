f3f3b51877328bfc8042bf6b2796237afcaef0c3
majorazero <daniel.cw.hsu@gmail.com>
2019-03-18 19:46:43 -0700

    added a user view page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
+
+  resources :users
 end
