5406398e4b698a5134cd22a26bed30269c6768e2
majorazero <daniel.cw.hsu@gmail.com>
2019-04-07 12:28:36 -0700

    added a before create activation token and a private downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save {self.email = email.downcase}
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
@@ -37,3 +39,15 @@ class User < ApplicationRecord
    end
 
 end
+
+
+private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
